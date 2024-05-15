03e88636cf4957b72840810f01e49aaf526601f2
anna <annagumiho190423@gmail.com>
2024-05-15 23:40:02 +0300

    changed index

--- current.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".slide").forEach(element => {
-    if(element.href === window.location.href) {
-        element.classList.add("active")
-    }
-
-    element.classList.remove("active");
-})
